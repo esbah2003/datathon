65c41e803cc880f385fbcebc2367d37f7acfb145
imrahn <35425886+ImrahnF@users.noreply.github.com>
2025-11-23 01:22:01 -0500

    restructure

--- src/mock_data/test.py
+++ src/mock_data/test.py
@@ -5,7 +5,9 @@ import pandas as pd
 
 
 here = Path(__file__).parent
-model_path = here / "model.joblib"
+model_path = here / "../model/model.joblib"
+model_path = model_path.resolve()
+
 
 if not model_path.exists():
     raise SystemExit("Model file not found. Run main.py to train and save the model first.")
